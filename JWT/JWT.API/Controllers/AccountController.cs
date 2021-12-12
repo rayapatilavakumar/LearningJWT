28978886b6ee0ed4741a1d043d414d67b69c39ac
Lava Kumar Rayapati <lrayapat@issquaredinc.com>
2021-12-12 22:44:00 +0530

    New logic added

---
+++ JWT/JWT.API/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JWT.API.Controllers
+{
+    public class AccountController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage ValidateLogin(string userName, string password)
+        {
+            if (userName =="admin" && password == "admin")
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, TokenManager.GenerateToken(userName));
+
+            }
+            return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "User Name / Password  is invalid");
+        }
+    }
+}
--- JWT/JWT.API/TokenManager.cs
+++ JWT/JWT.API/TokenManager.cs
@@ -11,17 +11,21 @@ namespace JWT.API
     public class TokenManager
     {
 
-        private static string SecretKey = "!QAZ@WSX#EDC4rfv";
+        private static string clientSecret = Base64Encode("!QAZ@WSX#EDC$RFV%TGB^YHN&UJM*IK<");
 
         public static string GenerateToken(string userName)
         {
-            byte[] key = Convert.FromBase64String(SecretKey);
+            byte[] key = Convert.FromBase64String(clientSecret);
 
             SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
 
             SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor
             {
-                Subject = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, userName, ClaimTypes.Email, "test@test.com") }),
+                Subject = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, userName),
+                    new Claim(ClaimTypes.Email,"test@test.com")
+
+                }),
                 Expires = DateTime.UtcNow.AddMinutes(30),
                 SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature)
             };
@@ -31,6 +35,57 @@ namespace JWT.API
             JwtSecurityToken token = handler.CreateJwtSecurityToken(descriptor);
 
             return handler.WriteToken(token);
+        }
+
+
+        public static ClaimsPrincipal GetPrincipal(string token)
+        {
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            JwtSecurityToken jwtToken = (JwtSecurityToken)tokenHandler.ReadToken(token);
+            if (jwtToken == null)
+                return null;
+            byte[] key = Convert.FromBase64String(clientSecret);
+
+            TokenValidationParameters parameters = new TokenValidationParameters()
+            {
+
+                RequireExpirationTime = true,
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                IssuerSigningKey = new SymmetricSecurityKey(key)
+            };
+
+            SecurityToken securityToken;
+            ClaimsPrincipal principal = tokenHandler.ValidateToken(token, parameters, out securityToken);
+
+            return principal;
+
+        }
+
+        public static string GetClaim(string token, string claimName)
+        {
+            string userName = null;
+
+            ClaimsPrincipal principal = GetPrincipal(token);
+            if (principal == null)
+                return null;
+
+            ClaimsIdentity identity = null;
+
+            identity = (ClaimsIdentity)principal.Identity;
+
+            Claim claim = identity.FindFirst(claimName);
+            
+            userName = claim.Value;
+
+            return userName;
+
+        }
+
+        private static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
         }
     }
 }
